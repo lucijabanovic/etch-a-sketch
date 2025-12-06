2002588f73eb671117529bf8fe1641e76deb29e0
lucijabanovic <lucija.banovic.27@dartmouth.edu>
2025-12-05 23:13:50 -0500

    Added a slider to change grid size.

--- .js
+++ .js
@@ -1,41 +1,56 @@
 let container = document.querySelector("#container");
 let styles = getComputedStyle(container);
-
-let numPixels = 100;
+let slider = document.querySelector("#slider");
+let sliderText = document.querySelector("#slide-container > p");
 
 let canvasSize = parseInt(styles.width);
-let pixelSize = canvasSize / numPixels;
+numPixels = slider.value;
 
-let isMouseDown = false;
+function drawGrid(numPixels) {
 
-for (let i = 0; i < numPixels; i++) {
-    let subContainer = document.createElement("div");
-    subContainer.style.width = `${canvasSize}px`;
-    subContainer.style.height = `${pixelSize}px`;
-    subContainer.style.display = 'flex';
-    container.appendChild(subContainer);
-    for (let j = 0; j < numPixels; j++) {
-        let pixel = document.createElement("div");
-        pixel.style.width = `${pixelSize}px`;
-        pixel.style.width = `${pixelSize}px`;
-        // pixel.style.border = '1px solid red';
-        subContainer.appendChild(pixel);
+    let pixelSize = canvasSize / numPixels;
+    let isMouseDown = false;
 
-        pixel.addEventListener("mousedown", () => {
-            pixel.style.backgroundColor = 'red';
-            isMouseDown = true;
-        });
+    for (let i = 0; i < numPixels; i++) {
+        let subContainer = document.createElement("div");
+        subContainer.style.width = `${canvasSize}px`;
+        subContainer.style.height = `${pixelSize}px`;
+        subContainer.style.display = 'flex';
+        container.appendChild(subContainer);
+        for (let j = 0; j < numPixels; j++) {
+            let pixel = document.createElement("div");
+            pixel.style.width = `${pixelSize}px`;
+            pixel.style.width = `${pixelSize}px`;
+            subContainer.appendChild(pixel);
 
-        pixel.addEventListener("mouseup", () => {
-            isMouseDown = false;
-        });
+            pixel.addEventListener("mousedown", () => {
+                pixel.style.backgroundColor = 'grey';
+                isMouseDown = true;
+            });
 
-        pixel.addEventListener("mouseover", () => {
-            if (isMouseDown) {
-                pixel.style.backgroundColor = 'red';
-            }
-        });
+            pixel.addEventListener("mouseup", () => {
+                isMouseDown = false;
+            });
+
+            pixel.addEventListener("mouseover", () => {
+                if (isMouseDown) {
+                    pixel.style.backgroundColor = 'grey';
+                }
+            });
+        }
     }
 }
+
+drawGrid(numPixels);
+
+slider.addEventListener("input", () => {
+    sliderText.textContent = `${slider.value}x${slider.value}`;
+    numPixels = slider.value;
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    drawGrid(numPixels);
+});
+
 
 
