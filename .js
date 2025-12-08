99879b7382418913933bf0fa0448f54d3e133f16
lucijabanovic <lucija.banovic.27@dartmouth.edu>
2025-12-07 19:36:59 -0500

    Added rainbow, darken, clear and erase button.

--- .js
+++ .js
@@ -2,11 +2,21 @@ let container = document.querySelector("#container");
 let styles = getComputedStyle(container);
 let slider = document.querySelector("#slider");
 let sliderText = document.querySelector("#slide-container > p");
+let rainbow = document.querySelector("#rainbow");
+let darken = document.querySelector("#darken");
+let buttons = document.querySelectorAll(".button");
+let erase = document.querySelector("#erase");
+let clearBtn = document.querySelector("#clear");
 
 let canvasSize = parseInt(styles.width);
-numPixels = slider.value;
+let numPixels = slider.value;
 
-function drawGrid(numPixels) {
+let isRainbow = false;
+let isDarken = false;
+let isErase = false;
+let prevNode = null;
+
+function drawGrid(numPixels, isRainbow, isDarken) {
 
     let pixelSize = canvasSize / numPixels;
     let isMouseDown = false;
@@ -22,9 +32,22 @@ function drawGrid(numPixels) {
             pixel.style.width = `${pixelSize}px`;
             pixel.style.width = `${pixelSize}px`;
             subContainer.appendChild(pixel);
+            
+            let darkStep = 0.1;
 
             pixel.addEventListener("mousedown", () => {
-                pixel.style.backgroundColor = 'grey';
+                if (isErase) {
+                    pixel.style.backgroundColor = 'white';
+                } else if (isRainbow) {
+                    pixel.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+                } else if (isDarken) {
+                    pixel.style.backgroundColor = `rgba(0, 0, 0, ${darkStep})`;
+                    if (darkStep < 1) {
+                        darkStep += 0.1;
+                    }
+                } else {
+                    pixel.style.backgroundColor = 'black';
+                }
                 isMouseDown = true;
             });
 
@@ -34,23 +57,101 @@ function drawGrid(numPixels) {
 
             pixel.addEventListener("mouseover", () => {
                 if (isMouseDown) {
-                    pixel.style.backgroundColor = 'grey';
+                    if (isErase) {
+                        pixel.style.backgroundColor = 'white';
+                    } else if (isRainbow) {
+                        pixel.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+                    } else if (isDarken) {
+                        pixel.style.backgroundColor = `rgba(0, 0, 0, ${darkStep})`;
+                        if (darkStep < 1) {
+                            darkStep += 0.1;
+                        }
+                    } else {
+                        pixel.style.backgroundColor = 'black';
+                    }
                 }
             });
         }
     }
 }
 
-drawGrid(numPixels);
+function clear() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+drawGrid(numPixels, isRainbow, isDarken);
 
 slider.addEventListener("input", () => {
     sliderText.textContent = `${slider.value}x${slider.value}`;
     numPixels = slider.value;
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
-    drawGrid(numPixels);
+    clear();
+    drawGrid(numPixels, isRainbow, isDarken);
 });
+
+clearBtn.addEventListener("click", () => {
+    clear();
+    drawGrid(numPixels, isRainbow, isDarken);
+});
+
+erase.addEventListener("click", () => {
+    if (isErase) {
+        erase.classList.remove("active");
+        if (prevNode != null) {
+            prevNode.classList.add("active");
+        }
+        isErase = false;
+    } else {
+        buttons.forEach((btn) => {
+            if (btn.classList.contains("active")) {
+                prevNode = btn;
+            }
+            btn.classList.remove("active");
+        });
+        erase.classList.add("active");
+        isRainbow = false;
+        isDarken = false;
+        isErase = true;
+    }
+});
+
+buttons.forEach(function(node) {
+    if (node.getAttribute("id") != "erase" && node.getAttribute("id") != "clear") {
+        node.addEventListener("click", () => {
+
+            if (node.classList.contains("active")) {
+                node.classList.remove("active");
+                isDarken = false;
+                isRainbow = false;
+                prevNode = null;
+            } else {
+                buttons.forEach((btn) => {
+                    btn.classList.remove("active");
+                });
+
+                node.classList.add("active");
+
+                if (node.getAttribute("id") == "rainbow") {
+                    isRainbow = true;
+                    isDarken = false;
+                    isErase = false;
+                } else if (node.getAttribute("id") == "darken") {
+                    isDarken = true;
+                    isRainbow = false;
+                    isErase = false;
+                }
+            }
+
+            clear();
+            drawGrid(numPixels, isRainbow, isDarken);
+
+        });
+    }   
+});
+
+
+
 
 
 
